a8b782634a9c00c2ad30734e02d53142111f935e
ar-bebop <aribeir4@gmail.com>
2023-09-16 20:13:26 -0400

    updated bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,47 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 if ! type stow > /dev/null 2>&1; then
     printf "Please install GNU stow."
     exit 1
 fi
 
-# CONFIG
-[ -d "$HOME/.config" ] || mkdir "$HOME/.config"
-stow --target="$HOME/.config" -R config
+usage() {
+    cat << EOF
+.../bootstrap.sh <argument>
 
+Arguments: (only one may be specified)
+
+install     stow all directories
+remove      unstow all directories
+EOF
+}
+# CONFIG
+install_config() {
+    [ -d "$HOME/.config" ] || mkdir "$HOME/.config"
+    stow --target="$HOME/.config" -R config
+}
 # HOME
-stow --dotfiles -R home
+install_home() {
+    stow --dotfiles -R home
+}
+
+remove_all() {
+    stow --dotfiles -D config
+    stow --target="$HOME/.config" -D config
+}
+
+install_config
+
+case "$1" in
+    install)
+        install_config
+        install_home
+        ;;
+    remove)
+        remove_all
+        ;;
+    *)
+        printf "Invalid argument.\n"
+        usage
+        ;;
+esac
