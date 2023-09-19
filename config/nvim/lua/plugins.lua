c151cc852d7af2289177f233a2f4f63b772a399c
ar-bebop <aribeir4@gmail.com>
2023-09-19 01:00:28 -0400

    nvim plugin updates

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -138,7 +138,25 @@ lazy.setup {
         }
     },
     {
+        'mickael-menu/zk-nvim',
+        config = function()
+            require('zk').setup({
+                picker = 'telescope',
+                lsp = {
+                    config = {
+                        cmd = { 'zk', 'lsp' },
+                        name = 'zk'
+                    },
+                    auto_attach = {
+                        enabled = true,
+                        filetypes = { 'markdown' }
+                    }
+                }
+            })
+        end
+    },
+    {
         'uga-rosa/ccc.nvim',
         opts = { highlighter = { auto_enable = true, lsp = true } },
-    },
+    }
 }
