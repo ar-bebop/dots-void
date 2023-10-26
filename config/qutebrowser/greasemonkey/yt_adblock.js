b10cda143a487d1a63f1df56bc2acd9677196b7d
ar-bebop <aribeir4@gmail.com>
2023-10-26 02:34:20 -0400

    idk anymore

--- config/qutebrowser/greasemonkey/yt_adblock.js
+++ config/qutebrowser/greasemonkey/yt_adblock.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         Auto Skip YouTube Ads
-// @version      1.1.0
-// @description  Speed up and skip YouTube ads automatically
+// @name         Auto Skip YouTube Ads 
+// @version      1.0.0
+// @description  Speed up and skip YouTube ads automatically 
 // @author       jso8910
 // @match        *://*.youtube.com/*
 // @exclude      *://*.youtube.com/subscribe_embed?*
@@ -13,16 +13,6 @@ setInterval(() => {
     }
     const ad = [...document.querySelectorAll('.ad-showing')][0];
     if (ad) {
-        const video = document.querySelector('video')
-        video.muted = true;
-        video.hidden = true;
-
-        // This is not necessarily available right at the start
-        if(video.duration != NaN) {
-            video.currentTime = video.duration;
-        }
-
-        // 16 seems to be the highest rate that works, mostly this isn't needed
-        video.playbackRate = 10;
+        document.querySelector('video').playbackRate = 10;
     }
 }, 50)
