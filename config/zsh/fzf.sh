cd382b9a7fba4aa93218b95659d3df77c6b91e5e
ar-bebop <aribeir4@gmail.com>
2024-02-15 03:51:03 -0500

    config updates

--- config/zsh/fzf.sh
+++ config/zsh/fzf.sh
@@ -1,1 +1,1 @@
-export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --color=bg+:#0B0000,spinner:#BB0000,hl:#AC6A74,fg:#FFBDC6,header:#AC6A74,info:#900C0F,pointer:#540000,marker:#540000,fg+:#201216,prompt:#900C0F,hl+:#AC6A74"
+export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --color=bg+:#222222,spinner:#707070,hl:#484848,fg:#C5C5B2,header:#484848,info:#5D5D5D,pointer:#8A8C84,marker:#8A8C84,fg+:#4D4D4D,prompt:#5D5D5D,hl+:#484848"
