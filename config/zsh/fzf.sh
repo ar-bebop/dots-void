3969fe5028ef6cc808924d5510776b0aa41e5a1d
ar-bebop <aribeir4@gmail.com>
2024-02-19 15:40:43 -0500

    added wallpapers, minor config changes

--- config/zsh/fzf.sh
+++ config/zsh/fzf.sh
@@ -1,1 +1,1 @@
-export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --color=bg+:#222222,spinner:#707070,hl:#484848,fg:#C5C5B2,header:#484848,info:#5D5D5D,pointer:#8A8C84,marker:#8A8C84,fg+:#4D4D4D,prompt:#5D5D5D,hl+:#484848"
+export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --color=bg+:#050403,spinner:#8C796A,hl:#96938C,fg:#E9E5DF,header:#96938C,info:#534E42,pointer:#573A23,marker:#573A23,fg+:#413E34,prompt:#534E42,hl+:#96938C"
