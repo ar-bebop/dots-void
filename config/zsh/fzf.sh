ab167e6049dc4acd31c3223a36a2177350629c32
ar-bebop <aribeir4@gmail.com>
2024-02-21 15:56:45 -0500

    ignored fzf colors gen

--- config/zsh/fzf.sh
+++
@@ -1,1 +0,0 @@
-export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --color=bg+:#050403,spinner:#8C796A,hl:#96938C,fg:#E9E5DF,header:#96938C,info:#534E42,pointer:#573A23,marker:#573A23,fg+:#413E34,prompt:#534E42,hl+:#96938C"
